c147e01e97321d9c6e9790a3e3d4f3db01209001
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-12-12 00:47:17 +0300

    feat: headroom header

--- scripts/dropzone.js
+++ scripts/dropzone.js
@@ -4,21 +4,22 @@ const dropzoneOptions = {
     url: "/post",
     // Max upload filesize (mb)
     maxFilesize: 2,
-    //Only one file can be uploaded at once
+    // Only one file can be uploaded at once
     parallelUploads: 1,
-    //Remove previews
+    // Remove previews
     disablePreviews: true,
-    //Function that called on loaded file
+    // Function that called on loaded file
     accept: (file, _) => {
-        //Creating loaded file reader
+        // Creating loaded file reader
         const reader = new FileReader();
         reader.readAsText(file, "UTF-8");
         reader.onload = (e) => {
-            //Setting code area content to file content
+            // Setting code area content to file content
             codeArea.textContent = e.target.result.toString();
         }
     }
 }
 
-//Initializing dropzone on #load-button
+// Initializing dropzone on #load-button
 const dropzone = new Dropzone("#load-button", dropzoneOptions);
+dropzone.init();
