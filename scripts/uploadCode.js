f46d5aead7a74e82c54152992c086e509953ffa1
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-12-11 23:13:04 +0300

    feat: dropzone file upload

---
+++ scripts/uploadCode.js
@@ -0,0 +1,24 @@
+const codeArea = document.querySelector(".code-area");
+
+const dropzoneOptions = {
+    url: "/post",
+    // Max upload filesize (mb)
+    maxFilesize: 2,
+    //Only one file can be uploaded at once
+    parallelUploads: 1,
+    //Remove previews
+    disablePreviews: true,
+    //Function that called on loaded file
+    accept: (file, _) => {
+        //Creating loaded file reader
+        const reader = new FileReader();
+        reader.readAsText(file, "UTF-8");
+        reader.onload = (e) => {
+            //Setting code area content to file content
+            codeArea.textContent = e.target.result.toString();
+        }
+    }
+}
+
+//Initializing dropzone on #load-button
+const dropzone = new Dropzone("#load-button", dropzoneOptions);
